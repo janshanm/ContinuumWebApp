9823f969d1602f087b1d5c6c35b45d9bc6771ffe
Brain Leke <brian@Brains-MacBook-Air.local>
2017-05-19 16:41:31 +0200

    Adding the binding to the controllers and also unit tests to cover the functionality

--- continuumAssessmentPlatform/strategy/strategy.js
+++ continuumAssessmentPlatform/strategy/strategy.js
@@ -9,6 +9,56 @@ angular.module('continuumAssessmentPlatform.strategy', ['ngRoute'])
   });
 }])
 
-.controller('StrategyCtrl', [function() {
+.controller('StrategyCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+  $scope.traveller1 = false;
+  $scope.artisan1 = false;
+  $scope.artisan2 = false;
+  $scope.artisan3 = false;
+  $scope.expert1 = false;
+  $scope.expert2 = false;
+  $scope.professional1 = false;
+  $scope.professional2 = false;
+  $scope.professional3 = false;
+  $scope.master1 = false;
+  $scope.master2 = false;
+  $scope.master3 = false;
+  $scope.master4 = false;
 
+  $scope.init = function () {
+      if(typeof $rootScope.assessments !== "undefined"){
+        var assessments = $rootScope.assessments;
+        var strategy = assessments['strategy'];
+        $scope.traveller1 = strategy['traveller1'];
+        $scope.artisan1 = strategy['artisan1'];
+        $scope.artisan2 = strategy['artisan2'];
+        $scope.artisan3 = strategy['artisan3'];
+        $scope.expert1 = strategy['expert1'];
+        $scope.expert2 = strategy['expert2'];
+        $scope.professional1 = strategy['professional1'];
+        $scope.professional2 = strategy['professional2'];
+        $scope.professional3 = strategy['professional3'];
+        $scope.master1 = strategy['master1'];
+        $scope.master2 = strategy['master2'];
+        $scope.master3 = strategy['master3'];
+        $scope.master4 = strategy['master4'];
+      }
+  };
+
+  $scope.saveAssessments = function(){
+      $rootScope.assessments = {'strategy': {
+        'traveller1': $scope.traveller1,
+          'artisan1': $scope.artisan1,
+          'artisan2': $scope.artisan2,
+          'artisan3': $scope.artisan3,
+          'expert1': $scope.expert1,
+          'expert2': $scope.expert2,
+          'professional1': $scope.professional1,
+          'professional2': $scope.professional2,
+          'professional3': $scope.professional3,
+          'master1': $scope.master1,
+          'master2': $scope.master2,
+          'master3': $scope.master3,
+          'master4': $scope.master4
+      }};
+  }
 }]);
--- continuumAssessmentPlatform/strategy/strategy_test.js
+++ continuumAssessmentPlatform/strategy/strategy_test.js
@@ -6,11 +6,144 @@ describe('continuumAssessmentPlatform.strategy module', function() {
 
   describe('strategy controller', function(){
 
-    it('should ....', inject(function($controller) {
-      //spec body
-      var strategyCtrl = $controller('StrategyCtrl');
-      expect(strategyCtrl).toBeDefined();
+    var controller;
+    var scope, rootScope;
+
+    beforeEach(inject(function($controller, $rootScope){
+      rootScope = $rootScope;
+      scope = rootScope.$new();
+      controller = $controller('StrategyCtrl', {'$scope': scope, '$rootScope': rootScope});
     }));
+
+    it('should ....', function() {
+      expect(controller).toBeDefined();
+    });
+
+    it('should have defaults as false for the traveller questions', function(){
+        expect(scope.traveller1).toBeDefined();
+        expect(scope.traveller1).toBeFalsy();
+    });
+
+    it('should have defaults as false for the artisan questions', function(){
+        expect(scope.artisan1).toBeDefined();
+        expect(scope.artisan2).toBeDefined();
+        expect(scope.artisan3).toBeDefined();
+
+        expect(scope.artisan1).toBeFalsy();
+        expect(scope.artisan2).toBeFalsy();
+        expect(scope.artisan3).toBeFalsy();
+    });
+
+    it('should have defaults as false for the expert questions', function(){
+        expect(scope.expert1).toBeDefined();
+        expect(scope.expert2).toBeDefined();
+
+        expect(scope.expert1).toBeFalsy();
+        expect(scope.expert2).toBeFalsy();
+    });
+
+    it('should have defaults as false for the professional questions', function(){
+        expect(scope.professional1).toBeDefined();
+        expect(scope.professional2).toBeDefined();
+        expect(scope.professional3).toBeDefined();
+
+        expect(scope.professional1).toBeFalsy();
+        expect(scope.professional2).toBeFalsy();
+        expect(scope.professional3).toBeFalsy();
+    });
+
+    it('should have defaults as false for the master questions', function(){
+        expect(scope.master1).toBeDefined();
+        expect(scope.master2).toBeDefined();
+        expect(scope.master3).toBeDefined();
+        expect(scope.master4).toBeDefined();
+
+        expect(scope.master1).toBeFalsy();
+        expect(scope.master2).toBeFalsy();
+        expect(scope.master3).toBeFalsy();
+        expect(scope.master4).toBeFalsy();
+    });
+
+    it('should set the default values based on the rootScope if they are present', inject(function($controller){
+        rootScope = {'assessments': {'strategy': {'traveller1': true, 'artisan1': true, 'artisan2': false, 'artisan3': true, 'expert1': true,
+        'expert2': false, 'professional1': false, 'professional2': false, 'professional3': false, 'master1': false,
+        'master2': false, 'master3': false}}};
+
+        controller = $controller('StrategyCtrl', {'$scope': scope, '$rootScope': rootScope});
+        scope.init();
+        expect(scope.traveller1).toBeTruthy();
+        expect(scope.artisan1).toBeTruthy();
+        expect(scope.artisan2).toBeFalsy();
+        expect(scope.artisan3).toBeTruthy();
+        expect(scope.expert1).toBeTruthy();
+        expect(scope.expert2).toBeFalsy();
+        expect(scope.professional1).toBeFalsy();
+        expect(scope.professional2).toBeFalsy();
+        expect(scope.professional3).toBeFalsy();
+        expect(scope.master1).toBeFalsy();
+        expect(scope.master2).toBeFalsy();
+        expect(scope.master3).toBeFalsy();
+        expect(scope.master4).toBeFalsy();
+    }));
+
+      it('should set the default values based on the initial values if rootScope not set', inject(function($controller){
+          rootScope = {};
+
+          controller = $controller('StrategyCtrl', {'$scope': scope, '$rootScope': rootScope});
+          scope.init();
+          expect(scope.traveller1).toBeFalsy();
+          expect(scope.artisan1).toBeFalsy();
+          expect(scope.artisan2).toBeFalsy();
+          expect(scope.artisan3).toBeFalsy();
+          expect(scope.expert1).toBeFalsy();
+          expect(scope.expert2).toBeFalsy();
+          expect(scope.professional1).toBeFalsy();
+          expect(scope.professional2).toBeFalsy();
+          expect(scope.professional3).toBeFalsy();
+          expect(scope.master1).toBeFalsy();
+          expect(scope.master2).toBeFalsy();
+          expect(scope.master3).toBeFalsy();
+          expect(scope.master4).toBeFalsy();
+      }));
+
+
+
+      it('should save the values for the assessment results for strategy', function(){
+        scope.traveller1 = true;
+        scope.artisan1 = true;
+        scope.artisan2 = false;
+        scope.artisan3 = false;
+        scope.expert1 = true;
+        scope.expert2 = true;
+        scope.professional1 = false;
+        scope.professional2 = false;
+        scope.professional3 = false;
+        scope.master1 = false;
+        scope.master2 = false;
+        scope.master3 = false;
+        scope.master4 = false;
+
+        var expectedStrategy = {'strategy': {'traveller1': true, 'artisan1': true, 'artisan2': false, 'artisan3': true, 'expert1': true,
+            'expert2': false, 'professional1': false, 'professional2': false, 'professional3': false, 'master1': false,
+            'master2': false, 'master3': false}};
+
+        scope.saveAssessments();
+
+        expect(rootScope.assessments['traveller1']).toEqual(expectedStrategy[['traveller1']]);
+        expect(rootScope.assessments['artisan1']).toEqual(expectedStrategy[['artisan1']]);
+        expect(rootScope.assessments['artisan2']).toEqual(expectedStrategy[['artisan2']]);
+        expect(rootScope.assessments['artisan3']).toEqual(expectedStrategy[['artisan3']]);
+        expect(rootScope.assessments['expert1']).toEqual(expectedStrategy[['expert1']]);
+        expect(rootScope.assessments['expert2']).toEqual(expectedStrategy[['expert2']]);
+        expect(rootScope.assessments['professional1']).toEqual(expectedStrategy[['professional1']]);
+        expect(rootScope.assessments['professional2']).toEqual(expectedStrategy[['professional2']]);
+        expect(rootScope.assessments['professional3']).toEqual(expectedStrategy[['professional3']]);
+        expect(rootScope.assessments['master1']).toEqual(expectedStrategy[['master1']]);
+        expect(rootScope.assessments['master2']).toEqual(expectedStrategy[['master2']]);
+        expect(rootScope.assessments['master3']).toEqual(expectedStrategy[['master3']]);
+        expect(rootScope.assessments['master4']).toEqual(expectedStrategy[['master4']]);
+
+    });
 
   });
 });
