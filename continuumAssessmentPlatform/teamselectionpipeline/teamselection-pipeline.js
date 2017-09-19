15290c57e48a368e5361bb9f782040f5c88a55b8
Brain Leke <brian@rfc.private.address.invalid.query>
2017-09-19 13:13:26 +0200

    Brain: Team Selection modification

--- continuumAssessmentPlatform/teamselectionpipeline/teamselection-pipeline.js
+++ continuumAssessmentPlatform/teamselectionpipeline/teamselection-pipeline.js
@@ -64,7 +64,7 @@ angular.module('continuumAssessmentPlatform.teamselection-pipeline', ['ngRoute']
                     {'name': 'Application Performance Monitoring'}, {'name': 'Aris'}, {'name': 'Business Applications'},
                     {'name': 'Mainframe Tools'}, {'name': 'Sharepoint'}, {'name': 'Source Code Management'},
                     {'name': 'Teamtrack'}, {'name': 'Technical Enablement'}, {'name': 'Tools and Support'}],
-                'Infra': [{'name': 'Sybase'}, {'name': 'DB2'}],
+                'Infra': [{'name': 'Sybase and DB2'}],
                 'PBB': [{'name': 'CDI/RSS'}, {'name': 'Natural and .NET Applications'}, {'name': 'Credit - Account Maintenance'},
                     {'name': 'Credit: Collections'}, {'name': 'Credit: Origination'}, {'name': 'E3'},
                     {'name': 'Customer Insights and Analytics'}, {'name': 'EIM Data Change'}, {'name': 'EIM Data Remediation'},
